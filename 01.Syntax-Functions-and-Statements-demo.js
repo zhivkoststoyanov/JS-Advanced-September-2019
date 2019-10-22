3c4146adf134df9e531e21d1a2a52dbe069886e1
zhivkoststyoanov <zhivkostoyanovbg@gmail.com>
2019-10-22 14:42:43 +0300

    Update 01.Syntax-Functions-and-Statements-demo.js

--- 01.Syntax-Functions-and-Statements-demo.js
+++ 01.Syntax-Functions-and-Statements-demo.js
@@ -1,15 +1,26 @@
-//
+//String Length
 
-function solve2 (...params){
-    let sum = params.reduce((a,b)=> a+ b.length, 0);
-    let avrg = Math.floor(sum / params.length);
+// function solve2 (...params){
+//     let sum = params.reduce((a,b)=> a+ b.length, 0);
+//     let avrg = Math.floor(sum / params.length);
 
-    return [sum, avrg]
+//     return [sum, avrg]
 
-};
+// };
 
+
+// console.log(
+//     solve2('chocolate', 'ice cream', 'cake')
+//     .join("\n")
+// )
+
+//	*Words Uppercase
+
+function solve(str){
+
+    return str.match(/\w+/gim).map(x=>x.toUpperCase()).join(", ");
+}
 
 console.log(
-    solve2('chocolate', 'ice cream', 'cake')
-    .join("\n")
-)
+    solve('Hi, how are you?')
+);
