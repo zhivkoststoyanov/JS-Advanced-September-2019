50d6c025458609afc6a36b37069552fc388f309e
zhivkoststyoanov <zhivkostoyanovbg@gmail.com>
2020-10-31 16:45:51 +0200

    Update 02.JS-Advanced-Objects-demo.js

--- 02.JS-Advanced-Objects-demo.js
+++ 02.JS-Advanced-Objects-demo.js
@@ -1,14 +1,16 @@
-function solve(){
+function solve(data){
+const isNotEmptyString = x => x !=="";
+const trimMyString = x => x.trim();
+const parseIfNumber = x => Number(x) ? Math.floor(Number(x)* 100)/ 100:x
+
 
 
 }
 
 console.log(
     solve([
-
-    
-    
+            '| Town | Latitude | Longitude |',
+            '| Sofia | 42.696552 | 23.32601 |',
+            '| Beijing | 39.913818 | 116.363625 |'
     ])
-
-
 );
